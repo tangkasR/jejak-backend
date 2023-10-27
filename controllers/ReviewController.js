6504042c430e0e95c66ee5820ca191fb9b6c2a28
tangkasR <tangkasrisdianto2@gmail.com>
2023-10-27 18:05:53 +0700

    menambah crud review

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -2,62 +2,73 @@ import ReviewModel from '../models/ReviewModel.js';
 import WisataModel from '../models/WisataModel.js';
 
 export const getReviewByWisataId = async (req, res) => {
-  //   try {
-  //     const response = await ReviewModel.findAll(
-  //       {
-  //         where: {
-  //           wisatumId: req.params.id
-  //         }
-  //       },
-  //       {
-  //         include: [
-  //           {
-  //             model: WisataModel
-  //           }
-  //         ]
-  //       }
-  //     );
-  //     res.status(200).json(response);
-  //   } catch (error) {
-  //     res.status(500).json({ msg: error.message });
-  //   }
+  try {
+    const response = await ReviewModel.findAll(
+      {
+        where: {
+          wisatumId: req.params.id
+        }
+      },
+      {
+        include: [
+          {
+            model: WisataModel
+          }
+        ]
+      }
+    );
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
 export const createReview = async (req, res) => {
-  //   const { name, review, wisatumId, rating } = req.body;
-  //   const findWisata = await WisataModel.findOne({
-  //     where: {
-  //       id: wisatumId
-  //     }
-  //   });
-  //   if (!findWisata) {
-  //     return res.status(404).json({ msg: 'wisata tidak ditemukan' });
-  //   }
-  //   let totalRating = findWisata.total_rating;
-  //   totalRating = totalRating + rating;
-  //   let totalRivewers = findWisata.total_reviewers + 1;
-  //   let averageRating = totalRating / totalRivewers;
-  //   await WisataModel.update(
-  //     {
-  //       nama_wisata: findWisata.nama_wisata,
-  //       lokasi: findWisata.lokasi,
-  //       total_rating: totalRating,
-  //       total_reviewers: totalRivewers,
-  //       rating: averageRating
-  //     },
-  //     {
-  //       where: {
-  //         id: findWisata.id
-  //       }
-  //     }
-  //   );
-  //   try {
-  //     await ReviewModel.create({
-  //       name: name,
-  //       review: review,
-  //       wisatumId: wisatumId
-  //     });
-  //     res.status(201).json({ msg: 'review berhasil ditambah' });
-  //   } catch (error) {
-  //     res.status(500).json({ msg: error.message });
-  //   }
+  const { name, review, rating } = req.body;
+  const findWisata = await WisataModel.findOne({
+    where: {
+      id: req.params.id
+    }
+  });
+  if (!findWisata) {
+    return res.status(404).json({ msg: 'wisata tidak ditemukan' });
+  }
+  let totalRating;
+  let totalRivewers;
+  if (findWisata.total_rating === null) {
+    totalRating = rating;
+    totalRivewers = 1;
+  } else {
+    totalRating = findWisata.total_rating + parseInt(rating);
+    totalRivewers = findWisata.total_viewers + 1;
+  }
+  const averageRating = totalRating / totalRivewers;
+  await WisataModel.update(
+    {
+      nama: findWisata.nama,
+      kategori: findWisata.kategori,
+      lokasi: findWisata.lokasi,
+      deskripsi: findWisata.deskripsi,
+      image: findWisata.image,
+      url: findWisata.url,
+      total_rating: totalRating,
+      total_viewers: totalRivewers,
+      rating: averageRating
+    },
+    {
+      where: {
+        id: findWisata.id
+      }
+    }
+  );
+  try {
+    await ReviewModel.create({
+      name: name,
+      review: review,
+      rating: rating,
+      wisatumId: findWisata.id
+    });
+    res.status(201).json({ msg: 'review berhasil ditambah' });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 };
--- routes/ReviewRoute.js
+++ routes/ReviewRoute.js
@@ -7,6 +7,6 @@ import {
 const router = express.Router();
 
 router.get('/getreviewbywisataid/:id', getReviewByWisataId);
-router.post('/createreview', createReview);
+router.post('/createreview/:id', createReview);
 
 export default router;
