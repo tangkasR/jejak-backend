fb308bb552438b1fe0d7dc5a8aa33ca9bcb5b5ae
tangkasR <tangkasrisdianto2@gmail.com>
2023-12-09 23:13:01 +0700

    updated

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -251,8 +251,14 @@ export const Login = async (req, res) => {
       }
     }
   );
+  res.header("Access-Control-Allow-Origin", "http://localhost:7007");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
   res.cookie("refreshToken", refreshToken, {
-    httpOnly: false,
+    httpOnly: true,
     maxAge: 60 * 60 * 1000
   });
   res.status(200).json(admin);
