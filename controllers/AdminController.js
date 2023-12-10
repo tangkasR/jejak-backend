46da8fb5be61fed232a66cc8888b9e38b441e99d
tangkasR <tangkasrisdianto2@gmail.com>
2023-12-11 00:00:41 +0700

    updated

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -252,7 +252,8 @@ export const Login = async (req, res) => {
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    maxAge: 3 * 60 * 1000
+    maxAge: 3 * 60 * 1000,
+    domain: "jejak-backend.vercel.app"
   });
   res.status(200).json(admin);
 };
