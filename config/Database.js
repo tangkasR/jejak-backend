4b908a0e2cca595be786151c3f3d27b99a747650
tangkasR <tangkasrisdianto2@gmail.com>
2023-12-11 23:24:39 +0700

    udpated

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,16 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 
-const db = new Sequelize("jejak_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql"
-});
+const db = new Sequelize(
+  "b8zdcb3owgysp6kesmmt",
+  "uvxqqszklwcnersc",
+  "pjp5xQF67wJyqs9lNULu",
+  {
+    host: "b8zdcb3owgysp6kesmmt-mysql.services.clever-cloud.com",
+    dialect: "mysql",
+    port: "3306",
+    dialectModule: mysql2
+  }
+);
 
 export default db;
